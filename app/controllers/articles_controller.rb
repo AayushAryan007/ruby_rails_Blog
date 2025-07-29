7cbf8c300cd7c567f8166796a970d021c49b5d7a
Aayush <unitedestates.ayush10@gmail.com>
2025-07-29 23:52:14 +0530

    create of crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles=Article.all
   end
@@ -7,4 +8,38 @@ class ArticlesController < ApplicationController
   def show
     @articles=Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+     
+    end
 end
+    # says:
+# “I expect the data to be nested under a key named article.”
+
+#  .permit(:title, :body)
+# This is the strong parameters part.
+
+# It only allows :title and :body to be extracted.
+
+# Any other fields (e.g., :is_admin) will be ignored and rejected.
+
+# This protects against users injecting unwanted fields.
+   
+    # strong parameters
+  
